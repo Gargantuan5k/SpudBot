a65694c7aa2b06a08fe47d845340d0aa74ecec21
Siddharth Vivek <gargantuan5k@outlook.com>
2021-06-23 21:43:03 +0530

    Add .env for config vars, delete unused dir ./bot-essentials/*, add wikipedia search support, add embeds for eightball command

--- bot.py
+++ bot.py
@@ -3,38 +3,29 @@ import random
 import datetime
 import pytz
 import wikipedia
+import os
+from decouple import config
 
 from mcstatus import MinecraftServer
 from discord.ext import commands
-# from prsaw import RandomStuff
-from math import e
 
-# rs = RandomStuff()
-client = commands.Bot(command_prefix="$")
+# prefix = config('DISCORD_BOT_PREFIX')
+prefix = config('DISCORD_BOT_TEST_PREFIX')    #! Test
+client = commands.Bot(command_prefix=prefix)
 tz = pytz.timezone("Asia/Calcutta")
-
-# def get_chat_response(message): 
-#     return rs.get_ai_response(message)
-
 
 @client.event
 async def on_ready():
     print("NNJGBot: Connection successful Status: READY")
 
-
-tok_file = open("bot-essentials/token.txt", "r")
-TOKEN = tok_file.read()
-
-#! DEBUG TOKEN (SpudBotTest) ln=28
-# TOKEN = "ODU2NTIzMTk3NDQ4MzIzMDky.YNCRYw.31FVbwh-wUKdEuGOZy_25zrtiiw"
+# TOKEN = config('DISCORD_BOT_TOKEN')
+TOKEN = config('DISCORD_BOT_TEST_TOKEN') #! Debug Bot Token
 
 print(TOKEN)
 
 def get_server_ping():
-    server = MinecraftServer.lookup(open("bot-essentials/nnjg-properties/server-ip.txt", "r").read())
-
-    #! DEBUG SERVER LOOKUP IP (ln=37)
-    # server = MinecraftServer.lookup("namehaven.aternos.me:34387")
+    server = MinecraftServer.lookup(os.environ.get("MCSERVER_IP"))
+    # server = os.environ.get("DEBUG_MCSERVER_IP") #! Debug MC server IP
 
     try:
         status = server.status()
@@ -76,19 +67,100 @@ async def ping(ctx):
     await ctx.send(message)
 
 
-@client.command(aliases=["8ball", "eight-ball", "prophecy", "answerme"])
-async def eightball(ctx, *, question):
-    res_list = [
-        "Yee", "Definitely", "Whag no", "Lmao no way", "Mayhaps", "Maybe idk",
-        "Why you askin' me\n I'm just a poor ~~boy~~ bot from a poor family\nSpare me my life from this monstrosity\nGALILEO GALILEO GALILEO FIGAR- ok ill stop",
-         "Seems likely", "Ye", "No idiot", "Yes idiot", "Why would you even ask that question?", "Yesn't", "Ok", "If you say so",
-        "69420", "Elephant", "My sources ~~google~~ say yes", "My sources ~~google~~ say no",
-        "Poor connection try again", "Mayhaps", "Uhhhhhh lemme see... lmao jk screw you", "cate", "doge",
-        "SPUDBOT_CRITICAL_ERROR: 001 STUPID_QUESTION", "Beep boop. Boop beep?", "Yes", "No", "Aye matey", "Nay matey",
-        "As ze French like to say it, \"Oui\".", "As ze French like to say it, \"Non\".", "Doubtful", "Truly g  ood sir",
-        "No I don't think I will.", "Get outta my room im playing minecraft", "Yes", "Yes", "Yes", "Yes", "Yes", "No", "No",
-        "No", "No", "No" 
-    ]
-    await ctx.send(f"{random.choice(res_list)}")
+@client.command(aliases=["8ball", "eight-ball", "prophecy", "spudball"])
+async def eightball(ctx, *, question=None):
+    if question is None or all(list(question)) == " ":
+        embed = discord.Embed(
+            title=":8ball: THE SPUDBALL SAYS: :8ball:",
+            description="_You didnt ask me a question lmao_",
+            color=discord.Color.blue(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.add_field(name="Command Syntax: ", value="`$8ball (your question goes here, without the brackts lol)`", inline=True)
+
+    else:
+        res_list = [
+            "Yee", "Definitely", "Whag no", "Lmao no way", "Mayhaps", "Maybe idk",
+            "Why you askin' me\n I'm just a poor ~~boy~~ bot from a poor family\nSpare me my life from this monstrosity\nGALILEO GALILEO GALILEO FIGAR- ok ill stop",
+            "Seems likely", "Ye", "No idiot", "Yes idiot", "Why would you even ask that question?", "Yesn't", "Ok", "If you say so",
+            "69420", "Elephant", "My sources ~~google~~ say yes", "My sources ~~google~~ say no",
+            "Poor connection try again", "Mayhaps", "Uhhhhhh lemme see... lmao jk screw you", "cate", "doge",
+            "SPUDBOT_CRITICAL_ERROR: 001 STUPID_QUESTION", "Beep boop. Boop beep?", "Yes", "No", "Aye matey", "Nay matey",
+            "As ze French like to say it, \"Oui\".", "As ze French like to say it, \"Non\".", "Doubtful", "Truly good sir",
+            "No I don't think I will.", "Get outta my room im playing minecraft", "Yes", "Yes", "Yes", "Yes", "Yes", "No", "No",
+            "No", "No", "No", ":look of superiority: P e r h a p s" 
+        ]
+        embed = discord.Embed(
+            title=":8ball: THE SPUDBALL SAYS: :8ball:",
+            description=f"_{random.choice(res_list)}_",
+            color=discord.Color.blue(),
+            timestamp=datetime.datetime.utcnow()
+        )
+    
+    embed.set_footer(text="SpudBot says hi :)")
+    embed.set_author(name="The Spudball")
+    await ctx.send(embed=embed)
+
+"""
+* AI/Chatbot
+"""
+
+def get_wiki_response(query):
+    try:
+        return wikipedia.summary(query, auto_suggest=False)
+    except Exception:
+        for new_query in wikipedia.search(query):
+            try:
+                return wikipedia.summary(new_query, auto_suggest=False)
+            except Exception:
+                pass
+    return "_I couldn't find anything about your question on Wikipedia :/_"
+
+@client.command()
+async def wiki(ctx, *, query):
+    response = get_wiki_response(query)
+
+    if len(response) <= 2048:
+        embed = discord.Embed(
+            title=query,
+            description=response,
+            color=discord.Color.blurple(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        
+        embed.set_author(name="SpudBot the All-Knowing")
+        embed.set_footer(text="Page 1/1 • SpudBot can search Wikipedia lol")
+
+        await ctx.send(embed=embed)
+    
+    else:
+        embed_list = []
+        n = 2048
+        embed_list = [response[i:i+n] for i in range(0, len(response), n)]
+        for num, item in enumerate(embed_list, start=1):
+            if (num == 1):
+                embed = discord.Embed(
+                    title=query,
+                    description=item,
+                    color=discord.Color.blurple()
+                )
+                embed.set_footer(text=f"Page {num} • SpudBot can search Wikipedia lol")
+                embed.set_author(name="SpudBot the All-Knowing")
+                await ctx.send(embed=embed)
+            else:
+                embed = discord.Embed(
+                    description=item,
+                    color=discord.Color.blurple()
+                )
+                embed.set_footer(text=f"Page {num} • SpudBot can search Wikipedia lol")
+                await ctx.send(embed=embed)
+
+"""
+* Moderation
+"""
+
+@client.command(aliases=["clr", "cc", "delete"])
+async def clear(ctx, amt=1):
+    await ctx.channel.purge(limit=amt+1)
 
 client.run(TOKEN)
